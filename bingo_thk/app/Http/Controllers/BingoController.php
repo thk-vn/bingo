e0f9170303a2e4290a89a5d1f506216573a459f6
p_quocgthang <p_quocthang@thk-hd.vn>
2025-10-20 09:54:04 +0700

    update

--- bingo_thk/app/Http/Controllers/BingoController.php
+++ bingo_thk/app/Http/Controllers/BingoController.php
@@ -2,10 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
-use App\Models\Game;
-
 class BingoController extends Controller
 {
     public function index()
