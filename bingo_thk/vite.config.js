f5034adeb530a02a259dec0b50b8c3ef0ba3f5c8
p_quocgthang <p_quocthang@thk-hd.vn>
2025-10-31 09:23:55 +0700

    remove vite

--- bingo_thk/vite.config.js
+++ bingo_thk/vite.config.js
@@ -19,12 +19,4 @@ export default defineConfig({
             refresh: true,
         }),
     ],
-    server: {
-        host: '0.0.0.0',
-        port: 5173,
-        hmr: {
-        host: '192.168.1.87:8080',
-        port: 5173,
-        },
-    },
 });
